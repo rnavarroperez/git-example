ee956d4432ed2573a712175e9dc82293dcc493e8
Rodrigo Navarro Perez <rnavarroperez@sdsu.edu>
2019-08-30 13:20:18 -0700

    Removed magic number and defined private parameter best_age_ever=33

--- src/evaluator.f90
+++ src/evaluator.f90
@@ -2,16 +2,18 @@ module evaluator
 implicit none
 private
 public evaluate_age
+integer , parameter :: best_age_ever=33
 contains
     subroutine evaluate_age(age, response)
         implicit none
         integer, intent(in) :: age
         character(len=*), intent(out) :: response
         real :: args
-        if (age == 33) then
+        if (age == best_age_ever) then
             response = "WOW, that's a great age"
         else
             response = "That's a nice age"
         endif
     end subroutine evaluate_age
+
 end module evaluator
